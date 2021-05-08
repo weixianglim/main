2bca4ea8a5c64c52f23091babb1ed4c4b878fd2b
SlifeX <44189259+SlifeX@users.noreply.github.com>
2021-05-08 18:22:24 +0800

    Apotcalypse writeup

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -23,15 +23,15 @@ const Projects = forwardRef((props, ref) =>
     // State variables
     const [arrCardsMain, ] = useState(
         [
-            {src: MindOverMatterTn, desc: "MindOverMatter is a 2D-puzzle platformer built from scratch on a custom 2D game engine. You play as a protagonist possessing the ability to freely toggle between 3 states - Solid, Liquid, and Gas. Each state allows you to maneuver through different environmental obstacles while providing you with unique skills and abilities.", label: "MindOverMatter", linkTo: "/MindOverMatter"},   
-            {src: TrappedTn, desc: "Trapped follows the life of a closeted protagonist as he tries to maneuver and escape through a dark maze. Throughout this journey, he is faced with societal judgement, fear and challenges. He has to make decisions along the maze which will lead him to different endings. Which route will he ultimately end up in?", label: "Trapped", linkTo: "/trapped"}          
+            {src: MindOverMatterTn, desc: "MindOverMatter is a 2D-puzzle platformer built from scratch on a custom 2D game engine. You play as a protagonist possessing the ability to freely toggle between 3 states - Solid, Liquid, and Gas. Each state allows you to maneuver through different environmental obstacles while providing you with unique skills and abilities.", label: "MindOverMatter", linkTo: "/MindOverMatter"},
+            {src: ApotcalypseTn, desc: "Apotcalypse is a colony simulator built on a custom 3D engine whereby the player has to manage a very crucial resource - Frulets! These cute bubbly creatures are indispensable and are necessary to stop the impending Apotcalypse.", label: "Apotcalypse", linkTo: "/apotcalypse"}
         ]
     );
     const [arrCardsRest, ] = useState(
         [
-            {src: ApotcalypseTn, desc: "Apotcalypse is a colony simulator built on a custom 3D engine whereby the player has to manage a very crucial resource - Frulets! These cute bubbly creatures are indispensable and are necessary to stop the impending Apotcalypse.", label: "Apotcalypse", linkTo: "/apotcalypse"},   
-            {src: FishieeTn, desc: "FishieProtecc is a fast-paced game which will strain the players reaction speeds and planning abilities. It is my freshman project in DigiPen and was nominated for Gumi Asia's Best Freshman Game award.", label: "FishieeProtecc", linkTo: "/fishieeprotecc"},   
-            {src: IllanTn, desc: "Illan: Shades of Memories is a 3D exploration game made in Unity Engine and is developed with the PS4 in mind as it's primary intended target platform.", label: "Illan: Shades of Memories", linkTo: "/illan"}               
+            {src: TrappedTn, desc: "Trapped follows the life of a closeted protagonist as he tries to maneuver and escape through a dark maze. Throughout this journey, he is faced with societal judgement, fear and challenges. He has to make decisions along the maze which will lead him to different endings. Which route will he ultimately end up in?", label: "Trapped", linkTo: "/trapped"},
+            {src: FishieeTn, desc: "FishieProtecc is a fast-paced game which will strain the players reaction speeds and planning abilities. It is my freshman project in DigiPen and was nominated for Gumi Asia's Best Freshman Game award.", label: "FishieeProtecc", linkTo: "/fishieeprotecc"},
+            {src: IllanTn, desc: "Illan: Shades of Memories is a 3D exploration game made in Unity Engine and is developed with the PS4 in mind as it's primary intended target platform.", label: "Illan: Shades of Memories", linkTo: "/illan"}
         ]
     );
 
--- src/components/pages/Projects/Apotcalypse.js
+++ src/components/pages/Projects/Apotcalypse.js
@@ -11,6 +11,13 @@ import { faMedal } from '@fortawesome/free-solid-svg-icons';
 // Import images
 import BannerImg from "../../../images/Projects/Apotcalypse/Apotcalypse.png"
 // import InfoScreen from "../../../images/Projects/MindOverMatter/SS1.png"
+import CircularPot from "../../../images/Projects/Apotcalypse/CircularPot.gif"
+import CircularQuadrant from "../../../images/Projects/Apotcalypse/CircularQuadrant.png"
+import CutsceneIntro from "../../../images/Projects/Apotcalypse/CutsceneIntro.gif"
+import NewEnvironment from "../../../images/Projects/Apotcalypse/NewEnvironment.gif"
+import FruletModel from "../../../images/Projects/Apotcalypse/FruletModel.png"
+import MainMenu from "../../../images/Projects/Apotcalypse/MainMenu.png"
+import MushroomGlow from "../../../images/Projects/Apotcalypse/MushroomGlow.gif"
 
 // Forward ref is used to propagate the element up to the parent.
 // Currently used to scroll from navbar to this element.
@@ -39,9 +46,9 @@ const Apotcalypse = forwardRef((props, ref) =>
                                                              OpenGL<br /><span className="endLine" />
 
                     <span className="tag">My Roles</span>Technical Lead<br /><span />
-                                                      Graphics Lead<br /><span />
-                                                      Engine Architecture Lead<br /><span />
-                                                      Technical Producer<br /><span className="endLine" />
+                                                         Graphics Lead<br /><span />
+                                                         Engine Architecture Lead<br /><span />
+                                                         Technical Producer<br /><span className="endLine" />
 
                     <span className="tag">Contributions</span>Game Engine Architecture<br /><span />
                                                               Graphics Environment (OpenGL)<br /><span />
@@ -71,9 +78,64 @@ const Apotcalypse = forwardRef((props, ref) =>
             </div> */}
             
             <h3>Overview</h3>
-             <p className="placeholderText">
-                 Full information will be provided soon, stay tuned!
+            <img src={MainMenu} alt="" />
+             <p>
+                 Apotcalypse was developed by a team of 9 members across 2 semesters. It is a colony simulator built on a custom 3D game engine whereby the player has to manage the game's crucial resource - Frulets.
+                 These Frulets are indispensable and are necessary to impede the "Apotcalypse".
+                 The main game loop revolves around managing the aforementioned Frulets, using them to craft various buildings and assigning them to work in different areas of the map. The main goal is to clear the
+                 infection levels of the different floors, mainly via the buildings available. Once all 4 floors of infections are cleared, the Apotcalypse will be prevented. 
             </p>
+            
+            <h3>Challenges</h3>
+            <img src={CutsceneIntro} alt="" />
+            <p>
+                The project started off pretty rough. For starters, the game idea itself was obscure; all of us on the programming team were very new to the genre and there were many misconceptions of the game idea
+                when it was pitched initially. We were also unsure of how the game will pan out in a 3D enrivonment as most existing games like "Oxygen Not Included" within the genre were 2D. Because of this, we ended up 
+                making several revamps and iterations throughout the course of this project which naturally ate up quite a large chunk of development time. As the technical lead, it was an arduous task to balance between engine development and 
+                constant integration of different iterations of the game concept into our custom engine. However, I'd say that it was quite useful. Thanks to the iteration process, we realised that
+                the initial game concept does not work well in a 3D environment which ultimately directed the team in the proper direction. While it was exhausting, it was completely necessary. I must say, the final product ended up being
+                something that I'm very sastisfied with. Being extremely iffy on the game idea when the project first started, my opinions have been pleasantly changed and Apotcalypse is now a project that I hold close to heart.
+            </p>
+            <h3>Iterations</h3>
+            <p>
+                The initial game concept was actually drastically different from the end product. We had a circular pot as our environment, and each floor was divided into 4 quadrants. Players were required to spin the pot around it's
+                center to get to the areas at the back.
+            </p>
+            <img src={CircularPot} alt="" />
+            <img src={CircularQuadrant} alt="" />
+            <p>
+                After several initial playtests and internal testing, it became apparent to us very quickly that the environment and game concept does not work well in 3D. The game revolves around resources and requires players to be
+                very reactive when an event happens in the game loop. Infection meter rising? Build a cleaner room quickly. Frulets are dying? Craft a hospital room immediately. The circular pot acted as a huge hindrance to this core 
+                mechanic - the entire environment is not fully visible to the player at any one time! When an event is happening at the back of the pot, the player might not be aware of it. Even if they were 
+                made aware, the process of rotating the pot to the back and getting to the correct floor is way too slow and were at many times too detrimental to come back from. Environmental maneuvering became a huge part of the gameplay
+                and was not an intended style of playing. With that, the team decided to scrap the current concept and revamped the environment.
+            </p>
+            <h3>The New Design</h3>
+            <p>
+                The environment was redesigned to allow players to have have a complete view of the map. The aspect of environment maneuver (rotating the pot) was completely scrapped and players no longer have to manipulate the map
+                to access their Frulets. Additionally, raycasting is now utilised as the primary way for players to engage with the Frulet resources - simply drag and drop them into the desired slots. The players are still able to
+                zoom into the individual rooms to engage with the Frulets, but the bulk of the game can be played via drag and drop. With this new revamp, the gameplay felt alot less taxing and we
+                eliminated the unintended parts of the gameplay we had before.
+            </p>
+            <img src={NewEnvironment} alt="" />
+            <h3>Art and Graphics</h3>
+            <p>
+                Apotcalypse was developed by a team without artists. Because of this, we have decided to head in the direction of a simplistic and cute art style with minimalistic models. We have also scattered animations and polish
+                on every Frulet action, including building, death, working (for the various buildings), etc.
+            </p>
+            <img src={FruletModel} alt="" />
+            <p>
+                On the engine side, we ended up utilising many post-processing effects like Bloom, glow and shadows, as well as particle effects to make up for the aesthetics. These graphical effects were also integrated into the
+                gameplay, being used as infection, spores and hover indicators. 
+                While the models and textures are minimalistic, the post-processing pipeline allowed the assets to still look aesthetically pleasing. 
+            </p>
+            <img src={MushroomGlow} alt="" />
+            <h3>Closing Thoughts</h3>
+            <p>
+                Developing Apotcalypse was a grueling but extremely rewarding process. Being my first 3D game developed in a custom engine, I have learnt alot of from this project ranging from the graphics pipeline (opengl, post-processing)
+                to the engine architecture. The end result of all our hard work is something I'm extremely proud of and I'd love to get into more of the development process. However, the best way to enjoy the game is to play
+                it yourself! If you're interested, feel free to download the installer of the game by following the link below.
+            </p><br />
         </React.Fragment>
     );
 
@@ -82,6 +144,7 @@ const Apotcalypse = forwardRef((props, ref) =>
         <ProjectEntry ref={ref} contentCallback={() => renderObj} 
         bannerImg={BannerImg} fadeScreen={true} 
         projectPrimaryTitle={projectTitle}
+        playLink="https://app.box.com/s/gp8xfj1r0tn2p44ygrj9zvhov0zf6943"
         btnText="View Full Info"/>
     );
 })
